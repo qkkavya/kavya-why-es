dbe81d1d4013b283c0b212275fae852e4465fad5
qkkavya <154304972+qkkavya@users.noreply.github.com>
2026-02-13 06:27:04 +0530

    Add files via upload

---
+++ agenda bsdk.exe/core/src/main/java/kavyaidk/java/BirdException.java
@@ -0,0 +1,7 @@
+package kavyaidk.java;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class BirdException extends Exception {
+  //logic implemented later
+}
---
+++ agenda bsdk.exe/core/src/main/java/kavyaidk/java/RedBird.java
@@ -0,0 +1,13 @@
+package kavyaidk.java;
+import com.badlogic.gdx.graphics.Texture;
+public class RedBird extends Bird{
+    public RedBird(Texture batch){
+        super(batch);
+    }
+    public void launch() throws BirdException{
+
+    }
+    public void hit() throws BirdException{
+
+    }
+}
